af26aae51ac315436dbf872cefb7f316c76aae45
Rafael <rauriac@iut.univ-paris8.fr>
2020-06-25 10:25:55 +0200

    SQL generation

--- src/main/java/fr/iut/makemydb/service/SqlService.java
+++ src/main/java/fr/iut/makemydb/service/SqlService.java
@@ -35,21 +35,31 @@ public class SqlService {
             Entity e1 = infos.findEntityByName(link1.getEntityName());
             Entity e2 = infos.findEntityByName(link2.getEntityName());
 
-            if( (link1.getCardinalMax().equals("1") && link2.getCardinalMax().equals("n")) ){
-                e2.getElement().getAttributes().add(
-                        new Attribute("fk_" + e1.findAttributePrimary().getName(),
-                                e1.findAttributePrimary(), e1));
-            }else if( (link2.getCardinalMax().equals("1") && link1.getCardinalMax().equals("n")) ){
-                e1.getElement()
+            if( link2.getCardinalMax().equals("1") && link1.getCardinalMax().equals("1") ){
+                ArrayList<Attribute> newAttributes = e1.getElement().getAttributes();
+                newAttributes.addAll(e2.getElement().getAttributes());
+                sqlEentities.remove(e1.getElement());
+                sqlEentities.remove(e2.getElement());
+                sqlEentities.add(new Element(relation.getElement().getName(), newAttributes));
+                System.out.print(sqlEentities);
+            }
+            if( link2.getCardinalMax().equals("n") && !link1.getCardinalMax().equals("n") ){
+                e1.getElement().getAttributes().add(
+                        new Attribute("fk_" + e2.findAttributePrimary().getName(),
+                                e2.findAttributePrimary(), e2));
+            }else if( link1.getCardinalMax().equals("n") && !link2.getCardinalMax().equals("n") ){
+                e2.getElement()
                         .getAttributes()
                         .add(new Attribute("fk_" +
-                                e2.findAttributePrimary().getName(),
-                                e2.findAttributePrimary(),
-                                e2));
+                                e1.findAttributePrimary().getName(),
+                                e1.findAttributePrimary(),
+                                e1));
             }else if( link1.getCardinalMax().equals("n") && link2.getCardinalMax().equals("n") ){
                 ArrayList<Attribute> newAttributes = relation.getElement().getAttributes();
                 Attribute fk1 = new Attribute(e1.findAttributePrimary().getName() + "_fk", e1.findAttributePrimary(), e1);
                 Attribute fk2 = new Attribute(e2.findAttributePrimary().getName() + "_fk", e2.findAttributePrimary(), e2);
+                newAttributes.add(fk1);
+                newAttributes.add(fk2);
                 newAttributes.add(infos.findEntityByName(link2.getEntityName()).findAttributePrimary());
                 newAttributes.add(infos.findEntityByName(link1.getEntityName()).findAttributePrimary());
                 sqlEentities.add(new Element(relation.getElement().getName(), newAttributes));
@@ -70,6 +80,9 @@ public class SqlService {
                 stringBuild.append(",");
             }
             stringBuild.append("\n\t\t" + attribute.getName() + " " + attribute.getType());
+            if(attribute.getTypeNumber() != 0){
+                stringBuild.append("(" + attribute.getTypeNumber() + ")");
+            }
         });
         stringBuild.append("," + "\n\t\t" + "PRIMARY KEY (");
 
